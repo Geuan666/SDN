599bbe28ecc1cabbe81ed9f15b018684c90796cb
Xijie Gong <2024091602008@std.uestc.edu.cn>
2025-05-12 17:30:29 +0800

    第二次提交

--- datacenter_controller.py
+++ datacenter_controller.py
@@ -6,18 +6,159 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import arp
+from ryu.lib.packet import icmp
+import ipaddress
 
 
-class HubController(app_manager.RyuApp):
+class ARPProxy:
     """
-    简单的集线器控制器 - 总是泛洪数据包
-    为数据中心网络提供最基本的连通性
+    ARP代理类：处理跨子网ARP请求并返回路由器MAC地址
     """
+
+    def __init__(self, logger):
+        self.logger = logger
+        self.arp_table = {}  # 存储IP到MAC的映射
+
+        # 路由器MAC地址
+        self.router_mac = "00:00:00:00:ff:01"
+
+        # 定义子网
+        self.subnets = {
+            "10.0.0.0/16": "外部网络",
+            "10.1.0.0/16": "数据中心内部网络"
+        }
+
+    def _get_subnet_for_ip(self, ip):
+        """确定IP地址所属的子网"""
+        ip_obj = ipaddress.ip_address(ip)
+        for subnet in self.subnets:
+            if ip_obj in ipaddress.ip_network(subnet):
+                return subnet
+        return None
+
+    def _are_ip_in_same_subnet(self, ip1, ip2):
+        """判断两个IP是否在同一子网"""
+        subnet1 = self._get_subnet_for_ip(ip1)
+        subnet2 = self._get_subnet_for_ip(ip2)
+        return subnet1 == subnet2
+
+    def learn_arp_entry(self, ip, mac):
+        """学习ARP表条目"""
+        self.arp_table[ip] = mac
+        self.logger.debug(f"ARP代理：学习ARP条目 {ip} -> {mac}")
+        return True
+
+    def get_mac_for_ip(self, ip):
+        """获取IP对应的MAC地址"""
+        return self.arp_table.get(ip, None)
+
+    def create_arp_reply(self, arp_req):
+        """
+        创建ARP回复数据包
+        arp_req: 原始ARP请求
+        """
+        pkt = packet.Packet()
+
+        # 查看请求和目标是否在同一子网
+        if not self._are_ip_in_same_subnet(arp_req.src_ip, arp_req.dst_ip):
+            # 跨子网请求，返回路由器MAC
+            reply_mac = self.router_mac
+            self.logger.info(f"ARP代理：跨子网请求 {arp_req.src_ip}->{arp_req.dst_ip}，返回路由器MAC")
+        elif arp_req.dst_ip in self.arp_table:
+            # 同子网且目标IP已知，返回实际MAC
+            reply_mac = self.arp_table[arp_req.dst_ip]
+            self.logger.info(f"ARP代理：同子网请求 {arp_req.src_ip}->{arp_req.dst_ip}，返回已知MAC")
+        else:
+            # 同子网但目标未知，无法响应
+            return None
+
+        # 构建以太网头
+        eth_hdr = ethernet.ethernet(
+            dst=arp_req.src_mac,
+            src=reply_mac,
+            ethertype=ether_types.ETH_TYPE_ARP
+        )
+
+        # 构建ARP回复
+        arp_reply = arp.arp(
+            hwtype=1, proto=0x0800, hlen=6, plen=4,
+            opcode=arp.ARP_REPLY,
+            src_mac=reply_mac,
+            src_ip=arp_req.dst_ip,
+            dst_mac=arp_req.src_mac,
+            dst_ip=arp_req.src_ip
+        )
+
+        pkt.add_protocol(eth_hdr)
+        pkt.add_protocol(arp_reply)
+        pkt.serialize()
+
+        return pkt.data
+
+    def handle_arp_packet(self, datapath, in_port, pkt):
+        """
+        处理ARP数据包
+        返回：(需要处理, 输出包数据, 输出端口)
+        """
+        arp_pkt = pkt.get_protocol(arp.arp)
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
+
+        # 学习源IP和MAC
+        self.learn_arp_entry(arp_pkt.src_ip, arp_pkt.src_mac)
+
+        # 如果是ARP请求
+        if arp_pkt.opcode == arp.ARP_REQUEST:
+            # 创建ARP回复
+            reply_data = self.create_arp_reply(arp_pkt)
+            if reply_data:
+                # 返回需要处理标志，回复数据，输出端口
+                return (True, reply_data, datapath.ofproto.OFPP_IN_PORT)
+
+        # 默认不处理
+        return (False, None, None)
+
+
+class EnhancedDCController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(HubController, self).__init__(*args, **kwargs)
-        self.logger.info("集线器控制器已启动")
+        super(EnhancedDCController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.arp_table = {}  # IP -> MAC
+        self.dp_to_dpid = {}  # Datapath -> DPID
+
+        # 初始化ARP代理
+        self.arp_proxy = ARPProxy(self.logger)
+
+        # 定义网关和网段映射
+        self.router_mac = "00:00:00:00:ff:01"  # 边缘路由器的虚拟MAC
+
+        # 子网信息
+        self.subnets = {
+            "10.0.0.0/16": "外部网络",
+            "10.1.0.0/16": "数据中心网络"
+        }
+
+        # 边缘路由器信息
+        self.edge_router_dpid = 2  # ed的dpid值
+
+        self.logger.info("增强版数据中心控制器已启动")
+
+    def _get_subnet_for_ip(self, ip):
+        """确定IP地址所属的子网"""
+        ip_obj = ipaddress.ip_address(ip)
+        for subnet in self.subnets:
+            if ip_obj in ipaddress.ip_network(subnet):
+                return subnet
+        return None
+
+    def _are_ip_in_same_subnet(self, ip1, ip2):
+        """判断两个IP是否在同一子网"""
+        subnet1 = self._get_subnet_for_ip(ip1)
+        subnet2 = self._get_subnet_for_ip(ip2)
+        return subnet1 == subnet2
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -25,61 +166,154 @@ class HubController(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # 安装Table-miss流表 - 所有数据包发送到控制器
-        match = parser.OFPMatch()  # 匹配所有数据包
+        # 存储DPID映射
+        self.dp_to_dpid[datapath.id] = datapath
+
+        # 安装默认流表 - Table-miss
+        match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
+
         self.logger.info(f"交换机已连接: dpid={datapath.id}")
 
-    def add_flow(self, datapath, priority, match, actions):
-        """添加流表项到交换机"""
+    def add_flow(self, datapath, priority, match, actions, idle_timeout=0):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # 将actions构建成指令
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
-
-        # 创建流表修改消息
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
-        # 发送消息到交换机
+                                match=match, instructions=inst,
+                                idle_timeout=idle_timeout)
         datapath.send_msg(mod)
+
+    def _send_packet_out(self, datapath, in_port, out_port, data):
+        parser = datapath.ofproto_parser
+        actions = [parser.OFPActionOutput(out_port)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=datapath.ofproto.OFP_NO_BUFFER,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    def _send_arp_reply(self, datapath, in_port, arp_req, reply_mac):
+        """发送ARP回复"""
+        eth_pkt = packet.Packet()
+
+        # 构建以太网头
+        eth_hdr = ethernet.ethernet(dst=arp_req.src_mac,
+                                    src=reply_mac,
+                                    ethertype=ether_types.ETH_TYPE_ARP)
+
+        # 构建ARP回复
+        arp_reply = arp.arp(hwtype=1, proto=0x0800, hlen=6, plen=4,
+                            opcode=arp.ARP_REPLY,
+                            src_mac=reply_mac,
+                            src_ip=arp_req.dst_ip,
+                            dst_mac=arp_req.src_mac,
+                            dst_ip=arp_req.src_ip)
+
+        eth_pkt.add_protocol(eth_hdr)
+        eth_pkt.add_protocol(arp_reply)
+        eth_pkt.serialize()
+
+        self._send_packet_out(datapath, in_port, datapath.ofproto.OFPP_IN_PORT, eth_pkt.data)
+        self.logger.debug(f"发送ARP应答: {arp_req.dst_ip} --> {arp_req.src_ip}")
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        """处理收到的数据包: 简单地泛洪每个数据包"""
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
+        dpid = datapath.id
 
-        # 解析数据包
+        # 确保该交换机有一个MAC表条目
+        self.mac_to_port.setdefault(dpid, {})
+
+        # 提取数据包信息
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        eth_pkt = pkt.get_protocol(ethernet.ethernet)
 
-        # 忽略LLDP包
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+        if not eth_pkt:
             return
 
-        # 简单记录数据包信息
-        dst = eth.dst
-        src = eth.src
-        dpid = datapath.id
-        self.logger.debug(f"Packet in dpid={dpid}: {src} -> {dst} from port {in_port}")
+        # 忽略LLDP包
+        if eth_pkt.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
 
-        # 无论目的地址如何，始终泛洪所有数据包
-        out_port = ofproto.OFPP_FLOOD
+        dst_mac = eth_pkt.dst
+        src_mac = eth_pkt.src
+
+        # 学习源MAC和端口
+        self.mac_to_port[dpid][src_mac] = in_port
+
+        # 处理ARP包
+        if eth_pkt.ethertype == ether_types.ETH_TYPE_ARP:
+            arp_pkt = pkt.get_protocol(arp.arp)
+            if arp_pkt:
+                # 使用ARP代理处理
+                handled, reply_data, out_port = self.arp_proxy.handle_arp_packet(datapath, in_port, pkt)
+                if handled:
+                    self._send_packet_out(datapath, in_port, out_port, reply_data)
+                    return
+
+        # 处理IPv4包
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ip_pkt:
+            src_ip = ip_pkt.src
+            dst_ip = ip_pkt.dst
+            self.logger.debug(f"IPv4数据包: {src_ip} -> {dst_ip}")
+
+            # 检查跨子网通信
+            if not self._are_ip_in_same_subnet(src_ip, dst_ip):
+                self.logger.info(f"跨子网通信: {src_ip} -> {dst_ip}")
+
+                # 如果当前交换机是边缘路由器，处理路由逻辑
+                if dpid == self.edge_router_dpid:
+                    # 尝试获取目标MAC
+                    if dst_ip in self.arp_table:
+                        dst_mac = self.arp_table[dst_ip]
+                        if dst_mac in self.mac_to_port[dpid]:
+                            out_port = self.mac_to_port[dpid][dst_mac]
+                            actions = [parser.OFPActionOutput(out_port)]
+
+                            # 安装流表
+                            match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_IP,
+                                                    ipv4_src=src_ip, ipv4_dst=dst_ip)
+                            self.add_flow(datapath, 1, match, actions, idle_timeout=30)
+
+                            # 转发当前包
+                            data = None
+                            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                                data = msg.data
+
+                            out = parser.OFPPacketOut(
+                                datapath=datapath, buffer_id=msg.buffer_id,
+                                in_port=in_port, actions=actions, data=data)
+                            datapath.send_msg(out)
+                            return
+
+        # 决定输出端口
+        if dst_mac in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst_mac]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        # 构建动作
         actions = [parser.OFPActionOutput(out_port)]
 
-        # 如果有缓冲ID，使用它；否则，包含完整数据
+        # 安装流表 (仅对非泛洪)
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst_mac)
+            self.add_flow(datapath, 1, match, actions, idle_timeout=30)
+
+        # 无论如何，都转发当前包
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        # 创建输出消息并发送
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
+        out = parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id,
+            in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
