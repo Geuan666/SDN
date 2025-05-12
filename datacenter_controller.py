2d0fa7686ffb88203be5af9bae4c70587a331c89
Xijie Gong <2024091602008@std.uestc.edu.cn>
2025-05-12 16:55:36 +0800

    第二次提交

--- datacenter_controller.py
+++ datacenter_controller.py
@@ -6,18 +6,18 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
-from ryu.lib.packet import ipv4
-from ryu.lib.packet import arp
 
 
-class SimpleDCController(app_manager.RyuApp):
+class HubController(app_manager.RyuApp):
+    """
+    简单的集线器控制器 - 总是泛洪数据包
+    为数据中心网络提供最基本的连通性
+    """
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleDCController, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
-        self.arp_table = {}  # IP -> MAC
-        self.logger.info("数据中心控制器已启动")
+        super(HubController, self).__init__(*args, **kwargs)
+        self.logger.info("集线器控制器已启动")
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -25,116 +25,61 @@ class SimpleDCController(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # 安装默认流表 - Table-miss
-        match = parser.OFPMatch()
+        # 安装Table-miss流表 - 所有数据包发送到控制器
+        match = parser.OFPMatch()  # 匹配所有数据包
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
-
         self.logger.info(f"交换机已连接: dpid={datapath.id}")
 
-        # 初始化MAC表
-        self.mac_to_port.setdefault(datapath.id, {})
-
-    def add_flow(self, datapath, priority, match, actions, idle_timeout=0, hard_timeout=0):
+    def add_flow(self, datapath, priority, match, actions):
+        """添加流表项到交换机"""
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        # 将actions构建成指令
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
+
+        # 创建流表修改消息
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst,
-                                idle_timeout=idle_timeout,
-                                hard_timeout=hard_timeout)
+                                match=match, instructions=inst)
+        # 发送消息到交换机
         datapath.send_msg(mod)
-
-    def _send_packet_out(self, datapath, buffer_id, in_port, out_port, data):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        actions = [parser.OFPActionOutput(out_port)]
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
-
-    def _handle_arp(self, datapath, in_port, pkt):
-        # 提取ARP包
-        pkt_arp = pkt.get_protocol(arp.arp)
-        pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
-
-        if pkt_arp.opcode == arp.ARP_REQUEST:
-            # 记录源MAC和IP映射
-            self.arp_table[pkt_arp.src_ip] = pkt_arp.src_mac
-            self.logger.debug(f"学习ARP: {pkt_arp.src_ip} -> {pkt_arp.src_mac}")
-
-            # 如果目标IP在ARP表中，直接回复
-            if pkt_arp.dst_ip in self.arp_table:
-                dst_mac = self.arp_table[pkt_arp.dst_ip]
-                self.logger.debug(f"ARP响应: {pkt_arp.dst_ip} -> {dst_mac}")
-                # 处理现有流表等...
-
-        # 无论是否处理，都允许ARP包继续传播
-        return False
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+        """处理收到的数据包: 简单地泛洪每个数据包"""
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
-        dpid = datapath.id
 
         # 解析数据包
         pkt = packet.Packet(msg.data)
-        eth_pkt = pkt.get_protocol(ethernet.ethernet)
-
-        if not eth_pkt:
-            return
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         # 忽略LLDP包
-        if eth_pkt.ethertype == ether_types.ETH_TYPE_LLDP:
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
 
-        dst_mac = eth_pkt.dst
-        src_mac = eth_pkt.src
+        # 简单记录数据包信息
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        self.logger.debug(f"Packet in dpid={dpid}: {src} -> {dst} from port {in_port}")
 
-        # 学习源MAC和端口
-        self.mac_to_port[dpid][src_mac] = in_port
-
-        # 处理ARP包
-        if eth_pkt.ethertype == ether_types.ETH_TYPE_ARP:
-            arp_pkt = pkt.get_protocol(arp.arp)
-            if arp_pkt:
-                self.arp_table[arp_pkt.src_ip] = src_mac
-                self.logger.debug(f"学习ARP: {arp_pkt.src_ip} -> {src_mac}")
-
-        # 查找目标端口 - 如果找到MAC地址则使用，否则泛洪
+        # 无论目的地址如何，始终泛洪所有数据包
         out_port = ofproto.OFPP_FLOOD
-        if dst_mac in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst_mac]
-            self.logger.debug(f"找到目标端口: {dst_mac} -> {out_port}")
-
-        # 构建动作
         actions = [parser.OFPActionOutput(out_port)]
 
-        # 对单播包添加流表项以缓存MAC学习结果
-        is_multicast = (dst_mac == 'ff:ff:ff:ff:ff:ff' or
-                        dst_mac.startswith('01:00:5e') or
-                        dst_mac.startswith('33:33'))
-
-        if out_port != ofproto.OFPP_FLOOD and not is_multicast:
-            # 为单播目的地添加流表
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst_mac)
-            # 使用较短的超时以允许拓扑变化
-            self.add_flow(datapath, 1, match, actions, idle_timeout=30)
-
-        # 无论何种情况都发送当前包
+        # 如果有缓冲ID，使用它；否则，包含完整数据
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id,
-            in_port=in_port, actions=actions, data=data)
+        # 创建输出消息并发送
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
--- datacenter_topo.py
+++ datacenter_topo.py
@@ -19,53 +19,72 @@ def createDatacenterNet():
     info(f'*** 添加控制器 (IP: {controller_ip})\n')
     c0 = net.addController('c0', controller=RemoteController, ip=controller_ip, port=6633)
 
-    # 添加交换机 - 使用更简单的拓扑
-    info('*** 添加交换机\n')
-    # 边缘路由器/外部交换机
-    edge = net.addSwitch('edge', dpid='1')
+    # 添加交换机 - 外部网络
+    info('*** 添加外部网络交换机\n')
+    external_switch = net.addSwitch('ex', dpid='0000000000000001')
+
+    # 添加交换机 - 数据中心网络
+    info('*** 添加数据中心网络交换机\n')
+    # 边缘路由器
+    edge_router = net.addSwitch('ed', dpid='0000000000000002')
 
     # 汇聚层交换机
-    spine1 = net.addSwitch('s1', dpid='2')
-    spine2 = net.addSwitch('s2', dpid='3')
+    spine1 = net.addSwitch('s1', dpid='0000000000000003')
+    spine2 = net.addSwitch('s2', dpid='0000000000000004')
+    spine3 = net.addSwitch('s3', dpid='0000000000000005')
 
     # 接入层交换机
-    leaf1 = net.addSwitch('l1', dpid='4')
-    leaf2 = net.addSwitch('l2', dpid='5')
-    leaf3 = net.addSwitch('l3', dpid='6')
-    leaf4 = net.addSwitch('l4', dpid='7')
-    leaf5 = net.addSwitch('l5', dpid='8')
+    leaf1 = net.addSwitch('l1', dpid='0000000000000006')
+    leaf2 = net.addSwitch('l2', dpid='0000000000000007')
+    leaf3 = net.addSwitch('l3', dpid='0000000000000008')
+    leaf4 = net.addSwitch('l4', dpid='0000000000000009')
+    leaf5 = net.addSwitch('l5', dpid='000000000000000a')
 
-    # 添加主机 - 全部在同一个子网10.0.0.0/8
-    info('*** 添加主机\n')
-    # 外部主机
-    h6 = net.addHost('h6', mac='00:00:00:00:00:06', ip='10.0.6.1/8')
-    h7 = net.addHost('h7', mac='00:00:00:00:00:07', ip='10.0.7.1/8')
-    h8 = net.addHost('h8', mac='00:00:00:00:00:08', ip='10.0.8.1/8')
+    # 注意：统一所有主机到一个单一的平坦网络 10.0.0.0/8
+    info('*** 添加外部网络主机\n')
+    h6 = net.addHost('h6', mac='00:00:00:00:00:06', ip='10.0.0.6/8')
+    h7 = net.addHost('h7', mac='00:00:00:00:00:07', ip='10.0.0.7/8')
+    h8 = net.addHost('h8', mac='00:00:00:00:00:08', ip='10.0.0.8/8')
 
-    # 数据中心内部主机
-    h1a = net.addHost('h1a', mac='00:00:00:00:01:01', ip='10.0.1.1/8')
-    h1b = net.addHost('h1b', mac='00:00:00:00:01:02', ip='10.0.1.2/8')
-    h2a = net.addHost('h2a', mac='00:00:00:00:02:01', ip='10.0.2.1/8')
-    h2b = net.addHost('h2b', mac='00:00:00:00:02:02', ip='10.0.2.2/8')
-    h3a = net.addHost('h3a', mac='00:00:00:00:03:01', ip='10.0.3.1/8')
-    h3b = net.addHost('h3b', mac='00:00:00:00:03:02', ip='10.0.3.2/8')
-    h4a = net.addHost('h4a', mac='00:00:00:00:04:01', ip='10.0.4.1/8')
-    h4b = net.addHost('h4b', mac='00:00:00:00:04:02', ip='10.0.4.2/8')
-    h5a = net.addHost('h5a', mac='00:00:00:00:05:01', ip='10.0.5.1/8')
-    h5b = net.addHost('h5b', mac='00:00:00:00:05:02', ip='10.0.5.2/8')
-    h5c = net.addHost('h5c', mac='00:00:00:00:05:03', ip='10.0.5.3/8')
+    # 添加主机 - 数据中心网络主机
+    info('*** 添加数据中心网络主机\n')
+    # Leaf1连接的主机
+    h1a = net.addHost('h1a', mac='00:00:00:00:01:01', ip='10.0.0.11/8')
+    h1b = net.addHost('h1b', mac='00:00:00:00:01:02', ip='10.0.0.12/8')
 
-    # 创建链路 - 简化连接
-    info('*** 创建链路\n')
+    # Leaf2连接的主机
+    h2a = net.addHost('h2a', mac='00:00:00:00:02:01', ip='10.0.0.21/8')
+    h2b = net.addHost('h2b', mac='00:00:00:00:02:02', ip='10.0.0.22/8')
 
-    # 外部主机连接到边缘
-    net.addLink(edge, h6)
-    net.addLink(edge, h7)
-    net.addLink(edge, h8)
+    # Leaf3连接的主机
+    h3a = net.addHost('h3a', mac='00:00:00:00:03:01', ip='10.0.0.31/8')
+    h3b = net.addHost('h3b', mac='00:00:00:00:03:02', ip='10.0.0.32/8')
 
-    # 边缘连接到汇聚层
-    net.addLink(edge, spine1)
-    net.addLink(edge, spine2)
+    # Leaf4连接的主机
+    h4a = net.addHost('h4a', mac='00:00:00:00:04:01', ip='10.0.0.41/8')
+    h4b = net.addHost('h4b', mac='00:00:00:00:04:02', ip='10.0.0.42/8')
+
+    # Leaf5连接的清洗服务器
+    h5a = net.addHost('h5a', mac='00:00:00:00:05:01', ip='10.0.0.51/8')
+    h5b = net.addHost('h5b', mac='00:00:00:00:05:02', ip='10.0.0.52/8')
+    h5c = net.addHost('h5c', mac='00:00:00:00:05:03', ip='10.0.0.53/8')
+
+    # 创建链路 - 外部网络
+    info('*** 创建外部网络链路\n')
+    net.addLink(external_switch, h6)
+    net.addLink(external_switch, h7)
+    net.addLink(external_switch, h8)
+
+    # 创建链路 - 外部网络连接到数据中心
+    info('*** 创建外部网络到数据中心的链路\n')
+    net.addLink(external_switch, edge_router)
+
+    # 创建链路 - 数据中心内部
+    info('*** 创建数据中心内部链路\n')
+    # 边缘路由器连接汇聚层
+    net.addLink(edge_router, spine1)
+    net.addLink(edge_router, spine2)
+    net.addLink(edge_router, spine3)
 
     # 汇聚层连接接入层
     net.addLink(spine1, leaf1)
@@ -80,15 +99,25 @@ def createDatacenterNet():
     net.addLink(spine2, leaf4)
     net.addLink(spine2, leaf5)
 
+    net.addLink(spine3, leaf1)
+    net.addLink(spine3, leaf2)
+    net.addLink(spine3, leaf3)
+    net.addLink(spine3, leaf4)
+    net.addLink(spine3, leaf5)
+
     # 接入层连接主机
     net.addLink(leaf1, h1a)
     net.addLink(leaf1, h1b)
+
     net.addLink(leaf2, h2a)
     net.addLink(leaf2, h2b)
+
     net.addLink(leaf3, h3a)
     net.addLink(leaf3, h3b)
+
     net.addLink(leaf4, h4a)
     net.addLink(leaf4, h4b)
+
     net.addLink(leaf5, h5a)
     net.addLink(leaf5, h5b)
     net.addLink(leaf5, h5c)
@@ -96,13 +125,18 @@ def createDatacenterNet():
     # 启动网络
     info('*** 启动网络\n')
     net.build()
+
+    # 启动控制器
+    info('*** 启动控制器\n')
     c0.start()
 
     # 启动所有交换机
     info('*** 启动交换机\n')
-    edge.start([c0])
+    external_switch.start([c0])
+    edge_router.start([c0])
     spine1.start([c0])
     spine2.start([c0])
+    spine3.start([c0])
     leaf1.start([c0])
     leaf2.start([c0])
     leaf3.start([c0])
@@ -112,6 +146,19 @@ def createDatacenterNet():
     # 等待控制器连接
     info('*** 等待控制器连接\n')
     time.sleep(5)
+
+    # 配置交换机STP启用
+    info('*** 配置STP (避免环路)\n')
+    for s in net.switches:
+        s.cmd('ovs-vsctl set bridge {} stp_enable=true'.format(s.name))
+    time.sleep(2)  # 等待STP收敛
+
+    # 确保主机间通信
+    info('*** 触发初始ARP请求\n')
+    for h in net.hosts:
+        for target in net.hosts:
+            if h != target:
+                h.cmd('ping -c 1 -W 1 {} >/dev/null 2>&1 &'.format(target.IP()))
 
     return net
 
